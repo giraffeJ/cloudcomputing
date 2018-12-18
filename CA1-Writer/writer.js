83419b7149225bb8c2cd3afda2e48e7a7a199584
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2018-12-19 05:34:03 +0900

    mkdirSync added

--- CA1-Writer/writer.js
+++ CA1-Writer/writer.js
@@ -11,10 +11,15 @@ const FILE_PATH =  CACHE_PATH + '/.last';
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send("Hello world!\n")
+    res.send("Hello world!\n" + FILE_PATH)
 }).get('/:whatever', (req, res) => {
     const msg = `${Date.now()},${req.params.whatever}`;
     console.log(msg)
+    try{
+        fs.mkdirSync(CACHE_PATH);
+    }catch(e){
+        if(e.code!='EEXIST') throw e;
+    }
     fs.writeFile(FILE_PATH, msg, 'utf8', function(error, data){
         if(error) {throw error};
         console.log('Write Complete\n');
