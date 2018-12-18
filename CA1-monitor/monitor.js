ab7ec6c7e4e0c63640dc73de460935a42721ffba
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2018-12-19 07:11:24 +0900

    fs.watch:

--- CA1-monitor/monitor.js
+++ CA1-monitor/monitor.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
-const watch = require('node-watch');
-const async = require('async');
 
 const CACHE_PATH = process.argv[2] || '/data';
 const FILE_PATH = CACHE_PATH + '/.last';
 const LOG_PATH = CACHE_PATH + '/.log';
 
-watch(CACHE_PATH, { recursive: true }, function (evt, name) {
+fs.watch(CACHE_PATH, { recursive: true }, (evt, name) => {
     fs.readFile(FILE_PATH, 'utf8', function (error, data) {
         if (error) { throw error };
         data = '\n' + data;
