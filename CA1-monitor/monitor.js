f362bed864e71f69a6277f8693f9c3aadfb2e52b
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2018-12-19 06:57:40 +0900

    watch path changed

--- CA1-monitor/monitor.js
+++ CA1-monitor/monitor.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const watch = require('node-watch');
+const async = require('async');
 
-const CACHE_PATH =  process.argv[2] || '/data';
-const FILE_PATH =  CACHE_PATH + '/.last';
-const LOG_PATH =  CACHE_PATH + '/.log';
+const CACHE_PATH = process.argv[2] || '/data';
+const FILE_PATH = CACHE_PATH + '/.last';
+const LOG_PATH = CACHE_PATH + '/.log';
 
-watch(FILE_PATH, {recursive: true}, function(evt, name){
-
-    fs.readFile(FILE_PATH, 'utf8', function(error, data){
-        if(error) {throw error};
+watch(CACHE_PATH, { recursive: true }, function (evt, name) {
+    fs.readFile(FILE_PATH, 'utf8', function (error, data) {
+        if (error) { throw error };
         data = '\n' + data;
-        fs.appendFile(LOG_PATH, data, function(err){
-            if(err) throw err;
+        fs.appendFile(LOG_PATH, data, function (err) {
+            if (err) throw err;
             console.log('Successfully updated');
         });
     });
